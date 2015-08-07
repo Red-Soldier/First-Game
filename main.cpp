d45555de5e5433519575c8211a16e739377b9906
Red Soldier <HF.2552202@gmail.com>
2015-08-07 03:54:21 -0500

    Adding code

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <CONIO.H>
+
+int main(){
+    int x = 0;
+    do{
+        std::cout <<x<< std::endl;
+        x++;
+    }while(x<=10);
+}
